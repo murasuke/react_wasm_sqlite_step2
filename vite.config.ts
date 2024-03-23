2d6d9f2ccdd4f48d1176f97acb2db56dc533d4d4
tkyk.niimura <tkyk.niimura@gmai.com>
2024-03-23 22:22:58 +0900

    fix readme

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { comlink } from 'vite-plugin-comlink';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? 'react_wasm_sqlite_step1' : './',
   plugins: [comlink(), react()],
   server: {
     headers: {
